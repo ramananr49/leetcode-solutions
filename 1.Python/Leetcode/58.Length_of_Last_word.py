f2751e86fe7afeea658b8396ee144778180cb777
ramananr49 <venketramanan@live.in>
2026-01-16 22:29:06 +0530

    Leetcode Problem 58.Length of Last word in python

---
+++ 1.Python/Leetcode/58.Length_of_Last_word.py
@@ -0,0 +1,31 @@
+"""
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+A word is a maximal substring consisting of non-space characters only.
+
+Example 1:
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+
+Example 2:
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+
+Example 3:
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+"""
+
+s = "Hello World"
+s1 = "   fly me   to   the moon  "
+s2 = "luffy is still joyboy"
+
+def lengthOfLastWord(s):
+    words = s.strip().split(" ")
+    return len(words[-1])
+    
+print(lengthOfLastWord(s))
+print(lengthOfLastWord(s1))
+print(lengthOfLastWord(s2))
